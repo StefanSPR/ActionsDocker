64d046169ed6a079a85767e2384c2dc7f4313ae0
StefanSPR <renato.stefangg@gmail.com>
2025-08-01 23:28:51 -0300

    fix: stringConexao

--- database/db.go
+++ database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"log"
 	"os"
+
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,9 +14,15 @@ var (
 	err error
 )
 
-func ConectaComBancoDeDados() {	
-	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="os.Getenv("DBNAME")+" port="os.Getenv("PORT")+" sslmode=disable"
-    DB, err = gorm.Open(postgres.Open(stringDeConexao))
+func ConectaComBancoDeDados() {
+	stringDeConexao := "host=" + os.Getenv("HOST") +
+		" user=" + os.Getenv("USER") +
+		" password=" + os.Getenv("PASSWORD") +
+		" dbname=" + os.Getenv("DBNAME") +
+		" port=" + os.Getenv("PORT") +
+		" sslmode=disable"
+
+	DB, err = gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
 	}
